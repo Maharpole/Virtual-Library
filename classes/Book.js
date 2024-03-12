ef73ec992c52151453994a2173148a20c131adea
Chemeeka Bahe <bchemeeka@gmail.com>
2024-03-12 13:21:31 -0700

    media and book changes

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,27 @@
 // import the Media class:
-
+const Media = require(Media.js)
 // create your Book class:
+class Book{
+    constructor(author,numPages,rating){
+    this.author = author;
+    this.numPages = numPages;
+    this.rating= rating;
+    }
 
+    summary(){
+        return `Title: ${title}, Author: ${author}, Page Count: ${numPages}, Genre: ${genre}, Rating ${rating}`
+    }
+    static highestRating(books){
+    for (let i = 0; i < books.length; i++) {
+    let winner = {}
+    if(books[5] > winner[5]){
+        winner = book
+    }
+
+}
+    
+}
+   
+}
 // don't change below
 module.exports = Book;
--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,14 @@
 // create your Media class:
-
+class Media{
+    constructor(title,year,genre,totalMediaCount){
+    this.title = title;
+    this.genre = genre;
+    this.year = year;
+    this.totalMediaCount = totalMediaCount;
+    }
+    summary(){
+        return `Title: ${title}, Year: ${year}, Genre: ${genre} `
+    }
+}
 // uncomment below to export it:
 module.exports = Media;
